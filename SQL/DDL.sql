aaf6149f285fe0f3bf0114faa1a69c450223926a
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-05-19 12:01:41 -0300

    Adicionando ALTER TABLE

--- SQL/DDL.sql
+++ SQL/DDL.sql
@@ -1,3 +1,6 @@
+# DELETANDO O BANCO DE DADOS PARA CRIAR DO ZERO
+DROP DATABASE BANCO;
+
 # CRIANDO BANCO DE DADOS
 CREATE DATABASE BANCO;
 
@@ -18,21 +21,53 @@ CREATE TABLE cliente(
 	cli_telefone VARCHAR(11) NOT NULL
 );
 
-# CRIANDO A TABELA PEDIDO
-CREATE TABLE pedido(
-	ped_numero INT PRIMARY KEY,
-	ped_data DATE
-);
-
-# CRIANDO A TABELA PRODUTO
-CREATE TABLE produto(
-	prod_codigo INT PRIMARY KEY,
-	prod_nome VARCHAR(255),
-	prod_preco FLOAT
-);
-
 #CRIANDO A TABELA CATEGORIA
 CREATE TABLE categoria(
 	prod_catcodigo INT PRIMARY KEY AUTO_INCREMENT,
 	prod_catnome VARCHAR(255) NOT NULL UNIQUE
 );
+
+# CRIANDO A TABELA PRODUTO
+CREATE TABLE produto(
+        # O Código do produto poderia conter letras(0a12869bw)
+        # Ou poderia não ser AUTO_INCREMENT(012908829)
+	prod_codigo INT PRIMARY KEY AUTO_INCREMENT,
+	prod_nome VARCHAR(255) NOT NULL UNIQUE,
+	fk_prod_categoria INT NOT NULL,
+	prod_preco DECIMAL(5,2) NOT NULL
+);
+
+# CRIANDO A TABELA PEDIDO
+CREATE TABLE pedido(
+	ped_numero INT PRIMARY KEY AUTO_INCREMENT,
+	ped_data DATE NOT NULL,
+	fk_ped_cliente VARCHAR(11) NOT NULL
+);
+
+CREATE TABLE pedido_produto(
+	fk_pedido INT NOT NULL,
+	fk_produto INT NOT NULL
+);
+
+# ALTER TABLE
+ALTER TABLE produto 
+ADD CONSTRAINT fk_categoria 
+FOREIGN KEY (fk_prod_categoria) 
+REFERENCES categoria(prod_catcodigo);
+
+ALTER TABLE pedido
+ADD CONSTRAINT fk_cliente
+FOREIGN KEY (fk_ped_cliente) 
+REFERENCES cliente(cli_cpf);
+
+ALTER TABLE pedido_produto
+ADD CONSTRAINT fk_ped
+FOREIGN KEY (fk_pedido)
+REFERENCES pedido(ped_numero);
+
+ALTER TABLE pedido_produto
+ADD CONSTRAINT fk_prod
+FOREIGN KEY (fk_produto)
+REFERENCES produto(prod_codigo);
+
+SHOW TABLES;
--- SQL/DML.sql
+++ SQL/DML.sql
@@ -1,7 +1,39 @@
-INSERT 
-INTO cliente (cli_cpf,cli_nome,cli_rua,cli_numero,cli_bairro,cli_telefone) 
+# CATEGORIA
+INSERT INTO categoria (prod_catnome) 
+VALUES ("Bermuda");
+
+INSERT INTO categoria (prod_catnome) 
+VALUES ("Camiseta");
+
+# INSERT INTO
+
+# PRODUTOS
+INSERT INTO produto (prod_nome, fk_prod_categoria, prod_preco)
+VALUES ("Bermuda X", 1, 100.00);
+
+INSERT INTO produto (prod_nome, fk_prod_categoria, prod_preco)
+VALUES ("Camiseta X", 2, 100.00);
+
+INSERT INTO produto (prod_nome, fk_prod_categoria, prod_preco)
+VALUES ("Meia X", 4, 10.00);
+
+INSERT INTO produto (prod_nome, fk_prod_categoria, prod_preco)
+VALUES ("Jaqueta X", NULL, 10.00);
+
+# CLIENTE
+INSERT INTO cliente (cli_cpf,cli_nome,cli_rua,cli_numero,cli_bairro,cli_telefone) 
 VALUES (2222226664,"Carlos da Silva","Rua tal",100,"Nova Europa","14999999997");
 
-INSERT 
-INTO categoria (prod_catnome) 
-VALUES ("bermuda");
+# PEDIDO
+INSERT INTO pedido (ped_data, fk_ped_cliente)
+VALUES (2025-05-19, 1);
+
+
+# SELECT
+SELECT * FROM categoria;
+SELECT * FROM produto;
+SELECT * FROM pedido;
+SHOW TABLES;	
+
+# DELETE
+DROP TABLE pedido;
